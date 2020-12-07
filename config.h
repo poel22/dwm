c17815ac7299c4a326c23945de2aad87150419b6
poel22 <hoermannpaul@gmail.com>
2020-12-07 08:56:09 +0100

    fix keys on thinkpad

--- config.h
+++ config.h
@@ -1,12 +1,15 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
+static const char rofiparam[]       = "run";
+static const char terminal[]       = "xfce4-terminal";
+static const char shell[]       = "/usr/bin/fish";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -89,16 +92,21 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon };
+
+static const char *rofi[] = { "rofi", "-show", rofiparam };
+static const char *termcmd[] = { terminal, "-e", shell };
 
 #include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,			XK_q,		killclient,	{0} },
-	{ MODKEY,                       XK_j,		focusstack,	{.i = +1 } },
-	{ MODKEY,                       XK_k,		focusstack,	{.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_j,		movestack,	{.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_k,		movestack,	{.i = -1 } },
+	{ MODKEY|ShiftMask,			XK_q,		killclient,	{0} },
+	{ MODKEY,			XK_Return,		spawn,	{.v = termcmd } },
+	{ MODKEY,		XK_d,		spawn,	{.v = rofi } },
+	{ MODKEY|ShiftMask,                       XK_j,		focusstack,	{.i = +1 } },
+	{ MODKEY|ShiftMask,                       XK_k,		focusstack,	{.i = -1 } },
+	{ MODKEY,             XK_j,		movestack,	{.i = +1 } },
+	{ MODKEY,             XK_k,		movestack,	{.i = -1 } },
 	{ MODKEY,			XK_space,	zoom,		{0} },
 	{ MODKEY,                       XK_h,		setmfact,	{.f = -0.05} },
 	{ MODKEY,                       XK_l,		setmfact,	{.f = +0.05} },
@@ -130,7 +138,7 @@ static Key keys[] = {
 	TAGKEYS(			XK_9,			8)
 	{ MODKEY,                       XK_0,		view,		{.ui = ~0 } },
 	{ MODKEY|ShiftMask,		XK_0,		tag,		{.ui = ~0 } },
-	{ MODKEY,			XK_F2,		quit,		{0} },
+	{ MODKEY|ShiftMask,			XK_e,		quit,		{0} },
 	{ MODKEY,			XK_g,		shiftview,	{ .i = -1 } },
 	{ MODKEY,			XK_semicolon,	shiftview,	{ .i = 1 } },
 	{ MODKEY,			XK_Page_Up,	shiftview,	{ .i = -1 } },
